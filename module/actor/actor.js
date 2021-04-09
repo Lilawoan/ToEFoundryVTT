46394663cadcd936f12fe829ad28e99a37060a08
Tom Pietri <tom.pietri-ext@pole-emploi.fr>
2021-04-09 14:45:03 +0200

    :bug: Fix default values not showing

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -13,25 +13,6 @@ export class TailsofequestriaActor extends Actor {
     const actorData = this.data;
     const data = actorData.data;
     const flags = actorData.flags;
-
-    // Make separate methods for each Actor type (character, npc, etc.) to keep
-    // things organized.
-    if (actorData.type === 'character') this._prepareCharacterData(actorData);
-  }
-
-  /**
-   * Prepare Character type specific data
-   */
-  _prepareCharacterData(actorData) {
-    const data = actorData.data;
-
-    // Make modifications to data here. For example:
-
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, ability] of Object.entries(data.abilities)) {
-      // Calculate the modifier using d20 rules.
-      ability.mod = Math.floor((ability.value - 10) / 2);
-    }
   }
 
 }
